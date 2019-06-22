7bec823c4af0681713ed2b9810091249c9fea63b
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 16:43:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  hash = {key1: "value1", key2: "value2"}
- 
-hash.each do |key, value|
+  
+birthday_kids.each do |key, value|
   puts "#{key}: #{value}"
 end
 
